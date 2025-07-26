1708dd0b4e5221f1d1f9a9ae98c63e748a451346
Vizonex <VizonexBusiness@gmail.com>
2025-07-26 13:18:32 -0500

    add Unpack to test & fixtures

---
+++ test/fixtures/a-implementation/code/unpack.ts
@@ -0,0 +1,8 @@
+import { code } from '../../../../src/frontend';
+import { Code } from './base';
+
+export class Unpack extends Code<code.Unpack> {
+  public build(): string {
+    return '';
+  }
+}
